b966ec1764576a340ed779dd453621bfc83e972f
Rohit Yadav <114162450+omshrohit@users.noreply.github.com>
2024-12-19 09:28:19 +0530

    Update data_cleaning.py
    the code will be easily execute in google colab and jupyter notebook

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,38 @@
+import pandas as pd
 
+weather=pd.read_csv('nyc_weather.csv',na_values=['T'])
+print(weather)
+
+weather.shape
+
+weather.head(1)
+
+weather.columns
+
+weather.info()
+
+weather.describe()
+
+weather.isnull().sum()
+
+weather.isnull().sum()/weather.shape[0]*100
+
+weather['EST']=pd.to_datetime(weather['EST'])
+
+type(weather['EST'][0])
+
+weather.set_index('EST',inplace=True)
+
+weather['WindSpeedMPH'].interpolate(method='time',inplace=True)
+
+weather['PrecipitationIn'].interpolate(method='time',inplace=True)
+
+weather[['WindSpeedMPH','PrecipitationIn']]
+
+weather['Events'].fillna('no event',inplace=True
+
+weather.isnull().sum()
+
+print('Data is ready to Data Analysis---')
+
+weather
